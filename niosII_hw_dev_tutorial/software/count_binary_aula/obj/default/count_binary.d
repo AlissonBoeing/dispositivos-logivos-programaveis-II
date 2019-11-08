c10f9e9bfd1a98d5ed2b1e42460942daa895e421
Alisson Boeing <alisson.b11@aluno.ifsc.edu.br>
2019-11-08 17:21:19 -0300

    Fazer programa led/chaves

---
+++ niosII_hw_dev_tutorial/software/count_binary_aula/obj/default/count_binary.d
@@ -0,0 +1,24 @@
+obj/default/count_binary.o: count_binary.c \
+ ../count_binary_aula_bsp//HAL/inc/alt_types.h \
+ ../count_binary_aula_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../count_binary_aula_bsp//HAL/inc/io.h \
+ ../count_binary_aula_bsp//HAL/inc/alt_types.h \
+ ../count_binary_aula_bsp//HAL/inc/sys/alt_irq.h \
+ ../count_binary_aula_bsp//HAL/inc/nios2.h \
+ ../count_binary_aula_bsp/system.h ../count_binary_aula_bsp/linker.h
+
+../count_binary_aula_bsp//HAL/inc/alt_types.h:
+
+../count_binary_aula_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../count_binary_aula_bsp//HAL/inc/io.h:
+
+../count_binary_aula_bsp//HAL/inc/alt_types.h:
+
+../count_binary_aula_bsp//HAL/inc/sys/alt_irq.h:
+
+../count_binary_aula_bsp//HAL/inc/nios2.h:
+
+../count_binary_aula_bsp/system.h:
+
+../count_binary_aula_bsp/linker.h:
